a681ab2bcf8a2fe94d7662cc7a953f66b1dbdd59
Damien Debin <damien.debin@gmail.com>
2026-03-24 17:42:19 +0100

    merge both commands

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,41 @@
 #!/usr/bin/env node
+/* eslint-disable max-params */
 import * as fs from 'fs'
-import { program } from 'commander'
+import { Argument, program } from 'commander'
 import { SSHAgentClient } from './lib/ssh_agent_client.js'
 
-program.name('ssh-crypt').description('Encryption through SSH Agent')
-
 program
-  .command('encrypt')
-  .description('Encrypt a file with your ssh-agent private key')
+  .name('ssh-crypt')
+  .description('Encrypt/Decrypt a file with your ssh-agent private key')
+  .addArgument(new Argument('<command>').choices(['encrypt', 'decrypt']))
   .argument('<source>', 'file to encrypt')
   .argument('[destination]', 'output path (default to stdout)')
   .requiredOption('-k, --key <string>', 'select the first matching pubkey in the ssh-agent')
   .requiredOption('-s, --seed <string>', 'is used to generate the secret')
-  .action(async (source, destination, options) => {
+  .action(async (action, source, destination, options) => {
     const agent = new SSHAgentClient()
     const key = await agent.getIdentity(options.key)
     if (!key) {
       throw new Error(`No SSH key found for "${options.key}"!`)
     }
-    const data = fs.readFileSync(source)
-    const output = await agent.encrypt(key, options.seed, data)
-    if (destination) {
-      fs.writeFileSync(destination, output)
-    } else {
-      process.stdout.write(output)
+    const output = async () => {
+      switch (action) {
+        case 'encrypt': {
+          const data = fs.readFileSync(source)
+          return agent.encrypt(key, options.seed, data).then(encrypted => Buffer.from(encrypted, 'ascii'))
+        }
+        case 'decrypt': {
+          const data = fs.readFileSync(source, { encoding: 'ascii' })
+          return agent.decrypt(key, options.seed, data)
+        }
+        default:
+          throw new Error('Unknwon action!')
+      }
     }
-  })
-
-program
-  .command('decrypt')
-  .description('Decrypt a file with your ssh-agent private key')
-  .argument('<source>', 'file to decrypt')
-  .argument('[destination]', 'output path (default to stdout)')
-  .requiredOption('-k, --key <string>', 'select the first matching pubkey in the ssh-agent')
-  .requiredOption('-s, --seed <string>', 'is used to generate the secret')
-  .action(async (source, destination, options) => {
-    const agent = new SSHAgentClient()
-    const key = await agent.getIdentity(options.key)
-    if (!key) {
-      throw new Error(`No SSH key found for "${options.key}"!`)
-    }
-    const data = fs.readFileSync(source, { encoding: 'ascii' })
-    const output = await agent.decrypt(key, options.seed, data)
     if (destination) {
-      fs.writeFileSync(destination, output)
+      fs.writeFileSync(destination, await output())
     } else {
-      process.stdout.write(output)
+      process.stdout.write(await output())
     }
   })
 
