8df1f21c4d96b4bc737e016fbe06fd7dd9eb2ae2
Damien Debin <damien.debin@gmail.com>
2026-04-13 14:13:05 +0200

    default to SHA2-512 for RSA signature

--- test/ssh_agent_stream.spec.ts
+++ test/ssh_agent_stream.spec.ts
@@ -14,7 +14,7 @@ const SEED = 'not_a_secret'
 describe('SSHAgentClient streams tests', () => {
   it('should encrypt', async () => {
     const agent = new SSHAgentClient()
-    const identity = await agent.getIdentity('key_rsa')
+    const identity = await agent.getIdentity('key_ed25519')
     if (!identity) {
       throw new Error()
     }
@@ -25,13 +25,13 @@ describe('SSHAgentClient streams tests', () => {
   })
   it('should decrypt', async () => {
     const agent = new SSHAgentClient()
-    const identity = await agent.getIdentity('key_rsa')
+    const identity = await agent.getIdentity('key_ed25519')
     if (!identity) {
       throw new Error()
     }
     const transform = await agent.getDecryptTransform(identity, SEED, 'hex')
     const stream = Readable.from(
-      'ecfd6bb57f4891ba7226886e90d2eb848022a495b15ffd91ffe760bca5605f9062c305ee14226d9daf7faa58460c8f50',
+      '2e306f1403b72eb17fe7187545dbd863be234ad1128ac3f34d60d102ced7bc43a7c4506d341463cff257b4d007b39143',
     )
     const decrypted = await text(stream.pipe(transform))
     chai.assert.strictEqual(decrypted, DECODED_STRING)
@@ -41,7 +41,7 @@ describe('SSHAgentClient streams tests', () => {
 describe('SSHAgentClient streams encodings tests', () => {
   it('should encrypt to base64', async () => {
     const agent = new SSHAgentClient()
-    const identity = await agent.getIdentity('key_rsa')
+    const identity = await agent.getIdentity('key_ed25519')
     if (!identity) {
       throw new Error()
     }
@@ -52,12 +52,12 @@ describe('SSHAgentClient streams encodings tests', () => {
   })
   it('should decrypt from base64', async () => {
     const agent = new SSHAgentClient()
-    const identity = await agent.getIdentity('key_rsa')
+    const identity = await agent.getIdentity('key_ed25519')
     if (!identity) {
       throw new Error()
     }
     const transform = await agent.getDecryptTransform(identity, SEED, 'base64')
-    const stream = Readable.from('8epe+B3bWcSGTPpyW2MRqHeAKjTj2NVnR4q1YNVB5LfXw9JE02wsb3RqZBTFbMXc')
+    const stream = Readable.from('5e0UbJX4+Rad+byLPnRNho3Qvjbeeqcwmg7yrXinHrTZ0788uuyvTl9jjbcpErF6')
     const decrypted = await text(stream.pipe(transform))
     chai.assert.strictEqual(decrypted, DECODED_STRING)
   })
